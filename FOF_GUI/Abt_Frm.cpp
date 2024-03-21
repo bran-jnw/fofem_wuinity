1c31a88542627bf21797723eaf0821e2c88244f4
bran-jnw <jonathan.wahlqvist@brand.lth.se>
2024-03-21 10:30:59 +0100

    Initial commit, restructure, does not compile

--- FOF_GUI/Abt_Frm.cpp
+++
@@ -1,2 +0,0 @@
-#include "Abt_Frm.h"
-
--- FOF_GUI/HTA-Old.h
+++
@@ -1,8 +0,0 @@
-/*************************************************
-* Name: HTA.h  Heat Time Arrays 
-*
-*************************************************/
-int HTA_Layers ();
-void HTA_Init ();
-void HTA_Put (int i_Lay, float temp, float moist, float psin, float t) ;
-int HTA_Get (int i_Lay, int iX, float *af_Heat, float *af_Moist, float *af_psin, float *af_Time);
